9d0d8d77d8da7eee5610e61461221b5a88eb62e1
jsperezging <jsperezging@gmail.com>
2023-01-19 18:05:49 -0500

    Archivos de configuración

---
+++ config/App.php
@@ -0,0 +1,5 @@
+<?php
+    const SERVERURL = "http://localhost/appGrowing/";
+    const COMPANY = "GROWIGN - APP";
+    const MONEDA = "$";
+    date_default_timezone_set("America/Bogota");
---
+++ config/Server.php
@@ -0,0 +1,12 @@
+<?php
+    const SERVER =  "localhost";
+    const DB = "";
+    const USER = "root";
+    const PSW = "";
+
+    const SGBD = "mysql:host=".SERVER.",dbname=".DB;
+
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY = '$GROWING@2023';
+    const SECRET_IV = '060401981';
+    
